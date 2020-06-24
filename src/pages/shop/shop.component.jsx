739b67d44f300d2f382de9d1a58ac4c643e87257
Tomas <tomasftrotti@gmail.com>
2020-06-24 12:19:38 -0300

    applied data normalization on shop data and shop selector, finished and styled collection overview component

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Route } from 'react-router-dom'
 
 import CollectionsOverview from '../../components/collections-overview/collections-overview.components'
-import CollectionPage from '../category/category.component'
+import CollectionPage from '../collection/collection.component'
 
 const ShopPage = ({ match }) => (
     <div className='shop-page'>
