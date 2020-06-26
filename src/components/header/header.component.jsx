51ce0cbc555b4196496d43065cd06c5f48dd2929
Tomas <tomasftrotti@gmail.com>
2020-06-26 18:04:16 -0300

    applied styled-components on custom-button styles

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -10,7 +10,10 @@ import { selectCurrentUser } from '../../redux/user/user.selectors'
 
 import { ReactComponent as Logo } from '../../assets/crown.svg'
 
-import { HeaderContainer, OptionDiv, OptionLink, OptionsContainer, LogoContainer } from './header.styles'
+import { HeaderContainer, 
+        OptionLink, 
+        OptionsContainer, 
+        LogoContainer } from './header.styles'
 
 const Header = ({ currentUser, hidden }) => (
     <HeaderContainer>
@@ -23,9 +26,9 @@ const Header = ({ currentUser, hidden }) => (
             </OptionLink>
             {
                 currentUser ?
-                <OptionDiv onClick={() => auth.signOut()}>
+                <OptionLink as='div' onClick={() => auth.signOut()}>
                 SIGN OUT
-                </OptionDiv>
+                </OptionLink>
                 :
                 <OptionLink className="option" to="/signin">
                 SIGN IN
