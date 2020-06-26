12472fde69df8d8732e01a046ea648af68cf4120
Tomas <tomasftrotti@gmail.com>
2020-06-26 18:59:44 -0300

    added media queries for mobile support

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { Switch, Route, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 
-import './App.css';
+import { GlobalStyles } from './global.styles'
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
@@ -47,6 +47,7 @@ class App extends React.Component {
   render() {
     return (
       <div>
+        <GlobalStyles />
         <Header />
         <Switch>
           <Route exact path='/' component={HomePage} />
